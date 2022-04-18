a6b0397f25bdb8b617a62b3b8528a4789c39dfab
shiekseeni421 <61022287+shiekseeni421@users.noreply.github.com>
2022-04-18 18:39:45 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+import MyContiner from "./FirstComponent.js";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header" >
+        <MyContiner />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/FirstComponent.js
@@ -0,0 +1,9 @@
+function MyContiner() {
+  return (
+    <>
+      <h1>Welcome ReactApp</h1>
+    </>
+  );
+}
+
+export default MyContiner;
